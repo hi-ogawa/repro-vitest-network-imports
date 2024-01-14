a40733189b981115cc7ed7ebc93cbaef12d1867c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-14 18:20:46 +0900

    refactor: use poolOptions.threads.execArgv

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    poolOptions: {
+      threads: {
+        execArgv: ["--experimental-network-imports"]
+      }
+    },
     server: {
       deps: {
         external: [/^https:/]
